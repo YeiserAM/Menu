767dfdcc42a433643f99fff13a5515be5eb704e0
Yeiser <48265047+YeiserAM@users.noreply.github.com>
2022-09-11 12:24:38 -0500

    update

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvus = findViewById(R.id.tvuser);
+        tvus.setText("Bienvenido: "+getIntent().getStringExtra("pupil"));
     }
 
     //METODO DEL BOTON RAIZ
